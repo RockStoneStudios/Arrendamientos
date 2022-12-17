8c2b4e7112d264e2476ba370502d8612f690e50e
RockStone <RockStoneStudios666@gmail.com>
2022-12-17 17:43:35 -0500

    Deployment

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const db = new Sequelize(process.env.DB_NOMBRE,process.env.DB_USUARIO,process.env.DB_PASSWORD ?? '',{
     host : process.env.DB_HOST,
+    port : process.env.DB_PORT,
     dialect : 'mysql',
     define :{
         timestamps : true
