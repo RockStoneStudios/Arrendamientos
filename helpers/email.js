6b9a7ef3d733a19ddaedd904055923040de2e841
RockStone <RockStoneStudios666@gmail.com>
2022-12-16 21:38:47 -0500

    End.

--- helpers/email.js
+++ helpers/email.js
@@ -6,6 +6,7 @@ dotenv.config();
 export const emailRegistro = async(datos)=>{
     var transport = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
+      service : 'gmail',
       port: process.env.EMAIL_PORT,
       auth: {
         user: process.env.EMAIL_USER,
